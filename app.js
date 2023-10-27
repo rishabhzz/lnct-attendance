d97470833317ab14184c45138fcfcf3fd473f853
Rishabh Singh <56092654+rishabhzz@users.noreply.github.com>
2023-10-28 01:16:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -101,7 +101,7 @@ app.get('/login', async (req, res) => {
 
  await page.type('#ctl00_cph1_txtStuUser', username);
  await page.type('#ctl00_cph1_txtStuPsw', password);
- await Promise.all([page.waitForNavigation({ timeout: 5000 }), page.click('#ctl00_cph1_btnStuLogin')]);
+ await Promise.all([page.waitForNavigation(), page.click('#ctl00_cph1_btnStuLogin')]);
 
 
  try {
